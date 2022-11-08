ec4e2fa5a2ac041c7b479c9cea67198da740a9f0
Michael Müller <Michael.Erich.Mueller@gmx.de>
2022-11-08 12:07:06 +0100

    Update

--- script_01.js
+++ script_01.js
@@ -10,3 +10,15 @@
 
 // Konsolen-Ausgaben
 // console.log("Hallo");
+// console.log(firstName);
+
+/***** 02 Deklaration + Wertzuweisung I *******/
+
+let firstName; // Deklaration (Definition)
+firstName = "Max"; // Wertzuweisung
+console.log(firstName); // Ausgabe
+
+let familyname = "Mütze" // Deklaration (Definition)
+console.log(familyname); // Ausgabe
+console.log(firstName + " " + familyname);
+
