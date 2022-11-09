dcc5653cc5fbc85acc43d04186c7ec6afe04589a
MichaelErichMueller <Michael.Erich.Mueller@gmx.de>
2022-11-09 09:32:39 +0100

    Update

--- script_01.js
+++ script_01.js
@@ -14,11 +14,17 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-let firstName; // Deklaration (Definition)
-firstName = "Max"; // Wertzuweisung
-console.log(firstName); // Ausgabe
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; // Wertzuweisung
+// console.log(firstName); // Ausgabe
 
-let familyname = "Mütze" // Deklaration (Definition)
-console.log(familyname); // Ausgabe
-console.log(firstName + " " + familyname);
+// let familyname = "Mütze" // Deklaration (Definition) + Wertuweißung
+// console.log(familyname); // Ausgabe
+// console.log(firstName + " " + familyname);
 
+
+
+let firstName, familyName; // Aufpassen auf die Groß und KleinSchreibung !!!
+firstName = prompt("Bitte Vornamen eingeben");
+familyName = prompt("Bitte Namen eingeben");
+console.log(firstName + " " + familyName);
