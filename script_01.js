f4d28bf05c43f1ce81fe94c32e508609c83c40b7
MichaelErichMueller <Michael.Erich.Mueller@gmx.de>
2022-11-09 09:34:26 +0100

    Update

--- script_01.js
+++ script_01.js
@@ -24,7 +24,23 @@
 
 
 
-let firstName, familyName; // Aufpassen auf die Groß und KleinSchreibung !!!
-firstName = prompt("Bitte Vornamen eingeben");
-familyName = prompt("Bitte Namen eingeben");
-console.log(firstName + " " + familyName);
+// let firstName, familyName; // Aufpassen auf die Groß und KleinSchreibung !!!
+// firstName = prompt("Bitte Vornamen eingeben");
+// familyName = prompt("Bitte Namen eingeben");
+// console.log(firstName + " " + familyName);
+
+// let test;
+// test = "Hi";
+// // test = 2;
+// // test = true;
+
+
+// // JS ist eine untypisierte Sprache !
+
+// console.log("Datentyp " + typeof test);
+// console.log("Inhalt: " + test) //Ausgabe
+
+
+const test = "Hallo";
+// test = "hi"; // KEINE neue Zuweisung zur Lz möglich ! --> Fehler
+console.log("Inhalt: " + test) //Ausgabe
